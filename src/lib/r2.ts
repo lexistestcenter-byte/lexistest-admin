a0361e465d86710ec7eb94b3878d6ce975e7bb0f
이준용 <junyong0751@gmail.com>
2026-02-20 16:49:32 +0900

    fix: fix

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -7,6 +7,7 @@ if (!process.env.R2_ACCOUNT_ID) {
 export const r2Client = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
